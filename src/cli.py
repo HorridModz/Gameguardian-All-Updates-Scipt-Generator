4b7576ae06ecbefbbe0a9eaa96afe074128ba586
HorridModz <105762560+HorridModz@users.noreply.github.com>
2025-08-01 20:44:44 -0400

    Fix bug in last commit - *actually* remove first argument if it is program name

--- src/cli.py
+++ src/cli.py
@@ -251,9 +251,9 @@ def print_result(message: str) -> None:
 def main(argv = None):
     if argv is None:
         argv = sys.argv[1:]
-    if argv[1] == "all_updates_generator":
+    if argv[0] == "all_updates_generator":
         # Program name should not be included in argv, so let's dump it
-        del argv[1]
+        del argv[0]
     if len(argv) == 1:
         # No arguments provided - show help
         print(__doc__)
