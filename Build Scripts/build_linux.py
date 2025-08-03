84dc9a781355ae6a87f1a31532265a72c6de954c
HorridModz <105762560+HorridModz@users.noreply.github.com>
2025-08-03 16:02:19 -0400

    Build - create target directory if it does not already exist to fix pyinstaller error

--- Build Scripts/build_linux.py
+++ Build Scripts/build_linux.py
@@ -16,6 +16,8 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(os.path.join(root_dir, "../src"))
 
 # Build with pyinstaller
+if not os.exists("../dist/build-temp"):
+    os.makedirs("../dist/build-temp")
 command = f'pyinstaller Linux/all_updates_generator.py' \
           ' --additional-hooks-dir="../Build Scripts/pyinstaller hooks"' \
           ' --add-data "resources/minified_script_template.lua:resources"' \
